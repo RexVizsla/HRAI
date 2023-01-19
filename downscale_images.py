37472953814df36e4bb822c9d4ae21cc4c038784
Anton <64529246+RexVizsla@users.noreply.github.com>
2023-01-19 12:35:23 +0100

    fixed some issues and saving files to a different folder

--- downscale_images.py
+++ downscale_images.py
@@ -12,10 +12,12 @@ def downscale_image(image_path, target_size=(2000, 2000)):
             new_size = (int(width*ratio), int(height*ratio))
             im = im.resize(new_size, resample=Image.LANCZOS)
             # Save the downscaled image
-            im.save(image_path)
-            print("Downscaled image with name '" + os.path(im) + "' to " + str(new_size))
+            filename, file_extension = os.path.splitext(os.path.basename(image_path))
+            im.save(os.path.join(image_path_downscaled,f"{filename}_downscaled.jpeg"))
+            print("Downscaled image with name '" + os.path.basename(im) + "' to " + str(new_size))
 
 image_path = r"C:\Users\rexvizsla\Desktop\AI\HRAI\possible_dataset\rated"
+image_path_downscaled = r"C:\Users\rexvizsla\Desktop\AI\HRAI\possible_dataset\rated\downscaled"
 
-for image in image_path:
-    downscale_image(image)
+for image in os.listdir(image_path):
+    downscale_image(os.path.join(image_path,image))
