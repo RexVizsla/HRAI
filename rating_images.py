decec1f24672868a2dcf27cadb5999cdf52b7035
Anton <64529246+RexVizsla@users.noreply.github.com>
2023-01-19 01:33:18 +0100

    Add files via upload
    improved version, big pictures get downscaled now

--- rating_images.py
+++ rating_images.py
@@ -4,6 +4,9 @@ from PIL import Image, ImageTk
 import tkinter as tk
 import shutil
 
+global json_path
+json_path = r"C:\Users\rexvizsla\Desktop\AI\HRAI\possible_dataset\rated"
+
 def on_submit():
     global rating
     rating = float(rating_entry.get())
@@ -16,32 +19,33 @@ def move_rated_image():
     shutil.move(os.path.join(folder, image_files[i]), os.path.join(r"possible_dataset\rated", image_files[i]))
 
 def save_rating():
-    data = {"rating": rating}
     json_file = os.path.splitext(image_files[i])[0] + ".json"
     json_path = os.path.join(r"possible_dataset\rated", json_file)
     with open(json_path, "w") as f:
-        json.dump(data, f)
+        json.dump({"rating": rating}, f)
 
 def show_next_image():
     global i
-    i += 1
+    while True:
+        i += 1
+        json_file = os.path.splitext(image_files[i])[0] + ".json"
+        json_path = os.path.join(r"C:\Users\rexvizsla\Desktop\AI\HRAI\possible_dataset\rated", json_file)
+        if not os.path.exists(json_path):
+            break
     if i >= len(image_files):
         root.destroy()
         return
-    # Checking if a json already exist isn't necessary because the pictures are in different folder.
-    # However I decided to not remove it because it helps avoid multiple ratings for one picture if something gets messed up.
-    while True:
-        json_file = os.path.splitext(image_files[i])[0] + ".json"
-        json_path = os.path.join(folder, json_file)
-        if not os.path.exists(json_path):
-            break
-        i += 1
     image_path = os.path.join(folder, image_files[i])
     image = Image.open(image_path)
+    width, height = image.size
+    ratio = min(2000/width, 1200/height)
+    new_size = (int(width*ratio), int(height*ratio))
+    image = image.resize(new_size, Image.LANCZOS)
     image = ImageTk.PhotoImage(image)
     label.config(image=image)
     label.image = image
     rating_entry.delete(0, tk.END)
+
 
 folder = r"C:\Users\rexvizsla\Desktop\AI\HRAI\possible_dataset\yet_to_be_rated"
 image_files = os.listdir(folder)
@@ -50,16 +54,13 @@ root = tk.Tk()
 root.title("Image Rating")
 
 i = -1
-image_path = os.path.join(folder, image_files[i])
-image = Image.open(image_path)
-image = ImageTk.PhotoImage(image)
-label = tk.Label(root, image=image)
+
+label = tk.Label(root)
 label.pack()
 
 rating_entry = tk.Entry(root)
 rating_entry.pack()
 rating_entry.bind("<Return>", lambda event: on_submit())
-
 
 show_next_image()
 
